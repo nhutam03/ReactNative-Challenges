6617a2c3d8e3b50247acc67e9448b3a9efedac87
nhutam03 <nhutam050@gmail.com>
2024-08-23 08:39:34 +0700

    week1 part1 developer skills and editor setup

---
+++ week1/developer_skill_editor_setup/dev_skill_editor_setup.js
@@ -0,0 +1,18 @@
+// Given an array of forecasted maximum temperatures, the thermometer displays a 
+// string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1 
+// days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+// Your tasks:
+// 1.  Create a function 'printForecast' which takes in an array 'arr' and logs a 
+// string like the above to the console. Try it with both test datasets.
+let printForecast = (arr) => {
+    let forecast = "";
+    for (let i = 0; i < arr.length; i++) {
+        forecast += `... ${arr[i]}ºC in ${i + 1} days `;
+    }
+    console.log(forecast);
+}
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
+
+// 2.  Use the problem-solving framework: Understand the problem and break it up 
+// into sub-problems!
