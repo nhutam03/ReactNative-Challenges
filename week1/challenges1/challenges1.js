ca10970055de736d07d56bf9df8ffdc1a3499fc1
H51M14\Student <Student@gmail.com>
2024-08-23 07:45:22 +0700

    week1 challenge1 part1 create variables

---
+++ week1/challenges1/challenges1.js
@@ -0,0 +1,15 @@
+// Mark and John are trying to compare their BMI (Body Mass Index), which is 
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+// and height in meter).
+
+// Your tasks:
+// 1.  Store Mark's and John's mass and height in variables
+// 2.  Calculate both their BMIs using the formula (you can even implement both versions)
+// 3.  Create a Boolean variable 'markHigherBMI' containing information about 
+// whether Mark has a higher BMI than John.
+
+let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let johnHeight = 1.95;
