4a085c03ef41501fe3ce0e76154fb5346bc21254
nhutam03 <nhutam050@gmail.com>
2024-08-30 13:44:40 +0700

    [UPDATE] Chỉnh sửa lại rewrite

--- week2/anyfunction/rewrite_function.js
+++ week2/anyfunction/rewrite_function.js
@@ -1,33 +1,75 @@
 //map -> tao ra 1 mang moi tu mang cu
 let arr = [3, 11, 2003]
-let arr2 = arr.map((value, index) => value + 1);
+Array.prototype.mapfn = function (callback) {
+    let newArr = [];
+    for (let i = 0; i < this.length; i++) {
+        newArr.push(callback(this[i], i));
+    }
+    return newArr;
+}
+let arr2 = arr.mapfn((value, index) => value + 1);
 console.log(arr2); // [4, 12, 2004]
 //filter -> loc ra cac phan tu theo dieu kien
-let arr3 = arr.filter((value, index) => value > 10);
+Array.prototype.filterfn = function (callback) {
+    let newArr = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i)) {
+            newArr.push(this[i]);
+        }
+    }
+    return newArr;
+}
+let arr3 = arr.filterfn((value, index) => value > 10);
 console.log(arr3); // [11, 2003]
-//reduce -> tinh toan gia tri cuoi cung cua mang
-let sum = arr.reduce((acc, value) => acc + value, 0);
-console.log(sum); // 2017
 //find -> tim ra phan tu dau tien theo dieu kien
-let first = arr.find((value, index) => value > 10);
-console.log(first); // 11
-//flat -> lam phang mang
-let arr4 = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-];
-let arr5 = arr4.flat();
-console.log(arr5); // [1, 2, 3, 4, 5, 6]
-//any -> kiem tra xem co phan tu nao theo dieu kien khong -> true/false
-let hasAny = arr.some((value, index) => value > 10);
-console.log(hasAny); // true
-//some -> kiem tra xem it nhat mot phan tu theo dieu kien -> true/false
-let all = arr.some((value, index) => value > 10);
-console.log(all); // true
-//flatmap -> lam phang mang va tao ra mang moi tu mang cu
-let arr6 = arr4.flatMap((value, index) => value.map((v, i) => v + 1));
-console.log(arr6); // [2, 3, 4, 5, 6, 7]
+Array.prototype.findfn = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i)) {
+            return this[i];
+        }
+    }
+}
+let arr4 = arr.findfn((value, index) => value > 10);
+console.log(arr4); // 11
+// //flatmap -> lam phang mang va tao ra mang moi tu mang cu
+Array.prototype.flatmapfn = function (callback) {
+    let newArr = [];
+    for (let i = 0; i < this.length; i++) {
+        newArr.push(callback(this[i], i));
+    }
+    return newArr.flat();
+}
+let arr5 = arr.flatmapfn((value, index) => [value, value + 1]);
+console.log(arr5); // [3, 4, 11, 12, 2003, 2004]
 //every -> kiem tra xem tat ca phan tu theo dieu kien -> true/false
-let all2 = arr.every((value, index) => value > 10);
-console.log(all2); // false
+Array.prototype.everyfn = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (!callback(this[i], i)) {
+            return false;
+        }
+    }
+    return true;
+}
+let arr6 = arr.everyfn((value, index) => value > 10);
+console.log(arr6); // false
+//some -> kiem tra xem co phan tu nao theo dieu kien -> true/false
+Array.prototype.somefn = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i)) {
+            return true;
+        }
+    }
+    return false;
+}
+let arr7 = arr.somefn((value, index) => value > 10);
+console.log(arr7); // true
+//reduce -> tinh toan gia tri cuoi cung dua tren tat ca phan tu
+Array.prototype.reducefn = function (callback, initialValue) {
+    let accumulator = initialValue;
+    for (let i = 0; i < this.length; i++) {
+        accumulator = callback(accumulator, this[i], i);
+    }
+    return accumulator;
+}
+let arr8 = arr.reducefn((accumulator, value, index) => accumulator + value, 0);
+console.log(arr8); // 2017
